996fd9692df8ff255abf6b3edde86c5ab635eef1
kobebryant2007 <kobebryant2007@163.com>
2017-05-22 21:25:43 +0800

    增加类

--- tdd/src/test/java/Frame.java
+++ tdd/src/test/java/Frame.java
@@ -2,11 +2,12 @@
  * Created by n on 2017/5/22.
  */
 public class Frame {
+    private int itsScore = 0;
     public int getScore(){
         return 0;
     }
 
     public void add(int pins){
-        
+        itsScore += pins;
     }
 }
